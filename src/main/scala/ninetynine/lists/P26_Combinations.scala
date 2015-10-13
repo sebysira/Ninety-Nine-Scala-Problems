e7cdb785ccee9d74b75f7468feea1cd6142f5570
Sebastiano Siragusa <seby.siragusa@gmail.com>
2015-10-13 11:19:04 +0200

    P026 problem

--- src/main/scala/ninetynine/lists/P26_Combinations.scala
+++ src/main/scala/ninetynine/lists/P26_Combinations.scala
@@ -1,8 +1,22 @@
 package ninetynine.lists
 
 /**
- * Created by seby on 13/10/15.
+ * Generate the combinations of K distinct objects chosen from the N elements of a list.
  */
 object P26_Combinations {
+
+    def flatMapSublist[A,B](l:List[A])(f:List[A]=>List[B]):List[B] = l match {
+      case Nil => Nil
+      case sublist@(_ :: tail) => f(sublist) ::: flatMapSublist(tail)(f)
+    }
+
+    def combinations[A](n:Int,l:List[A]):List[List[A]] = {
+      if(n==0)
+        List(Nil)
+      else
+        flatMapSublist(l){ sublist => combinations(n-1,sublist.tail).map(sublist.head :: _)
+    }
+
+  }
 
 }

ab6d8ebc04621def01b4a901e4a19d9f0db6e7f9
Sebastiano Siragusa <seby.siragusa@gmail.com>
2015-10-13 11:15:25 +0200

    fix P02_Spec

---
+++ src/main/scala/ninetynine/lists/P26_Combinations.scala
@@ -0,0 +1,8 @@
+package ninetynine.lists
+
+/**
+ * Created by seby on 13/10/15.
+ */
+object P26_Combinations {
+
+}
--- src/test/scala/ninetynine/lists/P02_Spec.scala
+++ src/test/scala/ninetynine/lists/P02_Spec.scala
@@ -6,7 +6,7 @@ import ninetynine.lists._
  */
 
 class P02_Spec extends FlatSpec with Matchers{
-  "P02_Last" should "Find the last but one element of a list" in {
+  "P02_Penultimate" should "Find the last but one element of a list" in {
     P02_Penultimate.penultimate(List(1,2,3)) should be (2)
     P02_Penultimate.penultimate(List('a', 'b', 'c', 'd')) should be ('c')
     P02_Penultimate.penultimate(List("mario", "giorgio", "alessio")) should be ("giorgio")
